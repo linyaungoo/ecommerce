add21ab96ac4fcb39c6186bc6f20bd5f1d329679
linyaungoo <linyaungoo3199@gmail.com>
2023-06-12 22:25:52 +0630

    first commit

---
+++ src/modules/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Hero from '../../components/Hero/Hero'
+import Products from '../../components/Products/Products'
+const Home = () => {
+  return (
+    <>
+        <Hero/>
+        <Products/>
+    </>
+  )
+}
+
+export default Home
